6e5f1b52404566c6e9dc4b8c51f02a39d6ae18da
Robin Hoogervorst <robin9975@gmail.com>
2016-08-27 20:32:43 +0200

    changes warning/error colors in logging

--- core/logger.py
+++ core/logger.py
@@ -1,7 +1,15 @@
 class Logger:
+    """ 
+    Class to log messages with different serverity levels,
+    supports notice, warning and error
+
+    Now just prints to the commandline,
+    may be improved to write to a log file
+    """ 
 
     ansi_reset = "\033[0m"
     ansi_red = "\033[31m"
+    ansi_yellow = "\033[33m"
 
     def __init__(self):
         pass
@@ -12,15 +20,21 @@ class Logger:
 
     def log_warning(self, message):
         """ log a warning message """
-        self.write_line(self.color_red("[warning]: ") + message)
+        self.write_line(self.color_yellow("[warning]: ") + message)
 
     def log_error(self, message):
         """ log an error message """
-        self.write_line("[error]: " + message)
+        self.write_line(self.color_red("[error]: ") + message)
 
     def write_line(self, message):
         """ outputs an log message, for now just prints to console """
         print(message)
 
     def color_red(self, message):
-        return "{}{}{}".format(self.ansi_red, message, self.ansi_reset)
+        return self.color(message, self.ansi_red)
+
+    def color_yellow(self, message):
+        return self.color(message, self.ansi_yellow)
+
+    def color(self, message, color):
+        return "{}{}{}".format(color, message, self.ansi_reset)
